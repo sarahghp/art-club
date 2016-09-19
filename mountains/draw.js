93ed33f6af33f2d3a946584537519c2a1ce1d8fc
Sarah GP <sarah.groff.palermo@gmail.com>
2016-09-19 15:12:45 -0400

    All three triangles; forget it pre–1820

--- mountains/draw.js
+++ mountains/draw.js
@@ -1,12 +1,12 @@
 console.log('Hi.');
 
-d3.json('../data-sets/norton.json', function(data){
+d3.json('../data-sets/norton_after1820.json', function(data){
   console.log(data);
 
   var svg = d3.select('svg'),
       w = +svg.attr('width'),
       h = +svg.attr('height'),
-      hRow = 275
+      hRow = 500
       margin = {
         top: 200,
         left: 200
@@ -30,24 +30,60 @@ d3.json('../data-sets/norton.json', function(data){
     console.log(data);
 
     var xScale = d3.scaleLinear()
-          .domain([1400, 2012])
+          .domain([1820, 2012])
           .range([0, w]),
         yScale = d3.scaleLinear()
-          .domain([0, 1900])
-          .range([0, hRow]);
+          .domain([0.1, 2000])
+          .range([hRow, 0]);
 
-    var points = function(d) {
-      return [[xScale(d.start), hRow], [xScale(d.start), hRow - yScale(d.authors.male.total_pages)], [xScale(d.end), hRow], [xScale(d.start), hRow]]
+    var mPoints = function(d) {
+      var end = xScale(d.end) - 20;
+      var arr = [[xScale(d.start), hRow], 
+                  [xScale(d.start), yScale(d.authors.male.total_pages)], 
+                  [end, hRow], 
+                  [xScale(d.start), hRow]];
+      return arr;
+    };
+
+    var fPoints = function(d) {
+      var start = xScale(d.start) + 20;
+      var arr = [[xScale(d.end), hRow], 
+                  [xScale(d.end), yScale(d.authors.female.total_pages)], 
+                  [start, hRow], 
+                  [xScale(d.end), hRow]];
+      return arr;
+    };
+
+    var tPoints = function(d){
+      var half = xScale(d.start) + (xScale(d.end) - xScale(d.start))/2;
+      var arr = [[xScale(d.start), hRow],
+                  [half, yScale(d.pages)],
+                  [xScale(d.end), hRow],
+                  [xScale(d.start), hRow]];
+
+      return arr;
     }
 
     var row = svg.append('g')
         .attr('class', 'row-'+ id);
 
-    row.selectAll('polygon')
+    row.selectAll('polygon-m')
         .data(data)
         .enter()
         .append('polygon')
-        .attr('points', points);
+        .attr('points', mPoints);
+
+    row.selectAll('polygon-f')
+        .data(data)
+        .enter()
+        .append('polygon')
+        .attr('points', fPoints);
+
+    row.selectAll('polygon-t')
+        .data(data)
+        .enter()
+        .append('polygon')
+        .attr('points', tPoints);
 
 
 
